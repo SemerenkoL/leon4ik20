074c4287e5dc7db5c9276a7ffa56a0bf493990f2
SemerenkoL <92167088+SemerenkoL@users.noreply.github.com>
2022-02-05 20:16:25 +0200

    Add files via upload

---
+++ RestExampleApplication.java
@@ -0,0 +1,10 @@
+package com.company;
+
+@SpringBootApplication
+public class RestExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestExampleApplication.class, args);
+    }
+
+}
